519e429e0af4375cfbaa33d12592992db260b076
Nitesh Kumar Sharma <104378178+nitesh0581@users.noreply.github.com>
2025-04-06 19:43:34 +0000

    chnge done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,3 +1,4 @@
+
 const myName="Nitesh";
 let city='Bareilly';
 var job='Dev';
@@ -5,6 +6,6 @@ var job='Dev';
 let currentlyWorking;
 console.table([myName,city,job='software developer']);
 console.log(myName);
-/**
- *
+/*
+ prefer not to use var because of block scope & functional scope
  */
